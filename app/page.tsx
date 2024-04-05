8e5efcaced8ac2699d25f0da46c98e5accc0b3cf
koushikkodukula <koushiks2000@gmail.com>
2024-04-05 10:45:36 +0100

    fixing resposnvieness

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="relative min-h-screen w-full flex flex-col items-center justify-center overflow-hidden">
+      {/* Background */}
       <div className="absolute inset-0 z-[-1]">
         <img
           src="/background.svg"
@@ -11,7 +12,13 @@ export default function Home() {
         />
       </div>
 
-      <p className="z-10 text-l text-gray-600 mt-32 -ml-10">
+      {/* Logo in the top-left corner */}
+      <div className="absolute top-0 left-0 z-10 p-5">
+        <img src="/logo.svg" alt="Logo" className="h-10 w-auto sm:h-12" />
+      </div>
+
+      {/* Text */}
+      <p className="z-10 text-base sm:text-lg text-gray-600 mt-32 -ml-10">
         Empowering Smart Decisions for Startups
       </p>
     </main>
